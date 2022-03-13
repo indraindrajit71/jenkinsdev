553239f42a8b0d70f1d798c3a4f831dd0e79f06b
indraindrajit71 <indraindrajit71@gmail.com>
2022-03-13 05:43:15 +0000

    3ndcommit_202203131101_Kumar

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /* This is a simple Java program. 
    FileName : "HelloWorld.java". */
-class HelloWorld.1
+class HelloWorld
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
